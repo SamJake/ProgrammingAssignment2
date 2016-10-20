fc0152c4135b722dc33fe1c8268a035679604e6e
Sam Jacob <Sam.Jacob@synechron.com>
2016-10-20 20:03:20 +0530

    Prog_Assign -v1

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(temp) inv <<- temp
+  getinv <- function() inv
+  list(set= set,
+       get= get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
@@ -12,4 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- l$getinv()
+  if(!is.null(i))
+  {
+    message("Extracting inverse matrix from cache....")
+    return(i)
+  }
+  m <- l$get()
+  i <- solve(m)
+  l$setinv(i)
+  i
 }
